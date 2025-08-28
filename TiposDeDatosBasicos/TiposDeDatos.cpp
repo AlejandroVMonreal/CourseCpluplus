4db5d91890c687486ae119b7c8aaf3c3fa57a511
AlejandroVMonreal <alexx111.al@hotmail.es>
2025-08-28 17:33:59 -0600

    Course c++
    make somethings about c++

---
+++ TiposDeDatosBasicos/TiposDeDatos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int numeros = 15;
+    float flotantes = 8.15;
+    double doble = 10.12345123;
+    char caracter = 'a';
+
+    cout << numeros << endl << flotantes << endl << doble << endl << caracter << endl;
+
+    return 0;
+}
